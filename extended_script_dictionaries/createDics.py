3385d2adea1360428476026984a886989306ff98
Christina Lohr <christina.lohr@uni-jena.de>
2020-07-17 16:59:57 +0200

    Update all code fragments about dictionary creation:
    * new Java package de.julielab.dictionaryhandling
        * RequestJuFiT.java
        * CreateLargeDictionary.java
    * revision of Python scripts handling dictionaries
    * update JuFiT jar

--- extended_script_dictionaries/createDics.py
+++ extended_script_dictionaries/createDics.py
@@ -1,29 +1,33 @@
 import glob
 import csv
 
-print('merge different UMLS dics')
+# before usage:
+# * create UMLS dictionaries with JuFiT
+# * create Gene dictionary with JULIELab/gene-name-mapping: https://zenodo.org/record/3874895#.XxG0Zh0aRhE
+# * adapt path names of your dictionaries
 
-def create_big_dic(dic_path, delim):
+def create_global_dict(dic_path, delim):
 	dics = glob.glob(dic_path + '/*')
-	big_dic = ''
+	global_dict = ''
 	for dic in dics:
 		name = dic.replace(dic_path + '/', '').replace('.txt', '').replace('.dict', '').replace('2019AB-', '').replace('-GER', '')
 		with open(dic) as tsvfile:
 			reader = csv.reader(tsvfile, delimiter=delim)
 			for row in reader:
 				#print(row[0])
-				big_dic += row[0] + '\t' + name + '\n'
-	return big_dic
+				global_dict += row[0] + '\t' + name + '\n'
+	return global_dict
 
+# todo apapt before usage
 path = '/the/name/of/the/path/with/dictionary/files'
 
 dic_path_umls = path + '/UMLS-semantic-group'
-big_dic_umls = create_big_dic(dic_path_umls, '|')
+global_dict_umls = create_global_dict(dic_path_umls, '|')
 
 dic_path_gene = path + '/gene'
-big_dic_gene = create_big_dic(dic_path_gene, '\t')
+global_dict_gene = create_global_dict(dic_path_gene, '\t')
 
-big_dic_file = open('bic_dic.txt', 'w')
-big_dic_file.write(big_dic_umls)
-big_dic_file.write(big_dic_gene)
-big_dic_file.close()
+global_dict_file = open('global_dictionary.txt.txt', 'w')
+global_dict_file.write(global_dict_umls)
+global_dict_file.write(global_dict_gene)
+global_dict_file.close()
--- extended_script_dictionaries/request-jufit.sh
+++ extended_script_dictionaries/request-jufit.sh
@@ -1,24 +1,8 @@
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=ACTI > dic/UMLS-2019AB-ACTI-GER.txt
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=ANAT > dic/UMLS-2019AB-ANAT-GER.txt
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=CHEM > dic/UMLS-2019AB-CHEM-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=CONC > dic/UMLS-2019AB-CONC-GER.txt
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=DEVI > dic/UMLS-2019AB-DEVI-GER.txt
 
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=DISO > dic/UMLS-2019AB-DISO-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=GENE > dic/UMLS-2019AB-GENE-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=GEOG > dic/UMLS-2019AB-GEOG-GER.txt
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=LIVB > dic/UMLS-2019AB-LIVB-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=OBJC > dic/UMLS-2019AB-OBJC-GER.txt
-
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=OCCU > dic/UMLS-2019AB-OCCU-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=ORGA > dic/UMLS-2019AB-ORGA-GER.txt
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=PHEN > dic/UMLS-2019AB-PHEN-GER.txt
 java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=PHYS > dic/UMLS-2019AB-PHYS-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=PROC > dic/UMLS-2019AB-PROC-GER.txt
-
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded > dic/UMLS-2019AB-GER.txt
-
-#Only the following semantic group names are supported:
-#ACTI, ANAT, CHEM, CONC, DEVI,
-#DISO, GENE, GEOG, LIVB, OBJC,
-#OCCU, ORGA, PHEN, PHYS, PROC
