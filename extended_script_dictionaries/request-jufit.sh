0f20829ee0615bde70b026dd8aee43cf7ea724ca
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 18:30:54 +0800

    Delete extended_script_dictionaries directory

--- extended_script_dictionaries/request-jufit.sh
+++
@@ -1,8 +0,0 @@
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=ANAT > dic/UMLS-2019AB-ANAT-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=CHEM > dic/UMLS-2019AB-CHEM-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=DEVI > dic/UMLS-2019AB-DEVI-GER.txt
-
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=DISO > dic/UMLS-2019AB-DISO-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=LIVB > dic/UMLS-2019AB-LIVB-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=PHEN > dic/UMLS-2019AB-PHEN-GER.txt
-java -jar JenaUmlsFilter-1.1-jar-with-dependencies.jar MRCONSO.RRF MRSTY.RRF GER --grounded --semanticGroup=PHYS > dic/UMLS-2019AB-PHYS-GER.txt
